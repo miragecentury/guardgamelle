a1277c40a4e9b74829876d843a3264e2581b19e4
dohantaur <dohant.aurelien@gmail.com>
2013-11-07 21:32:36 +0100

    defaultController insertion EvnetGamelle

--- src/Guard/Api/v1Bundle/Controller/DefaultController.php
+++ src/Guard/Api/v1Bundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace Guard\Api\v1Bundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\Constraints as Upsert;
+use Guard\Common\GamelleBundle\Entity\Gamelle;
+use Guard\Common\EventBundle\Entity\EventGamelle;
 
 class DefaultController extends Controller {
 
@@ -25,10 +27,15 @@ class DefaultController extends Controller {
                 $errorDateList = $this->get('validator')->validateValue($d->setTimestamp($request->date), $dateConstraint);
                 $errorStateList = $this->get('validator')->validateValue($request->state, $stateConstraint);
                 if (count($errorIdList) != 0 || count($errorDateList) != 0 || count($errorStateList) != 0 || $d->setTimestamp($request->date) > new \DateTime('NOW')) {
-                    return new response($errorDateList, 500);
+                    return new response("", 500); 
                 }
 
-
+                $eventGamelle = new EventGamelle();
+                $Gamelle = $this->getDoctrine()->getManager()->getRepository("GuardCommonGamelleBundle:Gamelle")->findOneBy(array('uid' => $request->id));
+                $eventGamelle->setId($Gamelle->id);
+                $eventGamelle->setDatetime((new \DateTime())->setTimestamp($request->date));
+                $this->getDoctrine()->getManager()->persist($Gamelle);
+                $this->getDoctrine()->getManager()->flush();
 
                 return new Response("", 200);
             } else {
