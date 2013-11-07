b49bd528988cdbd4d8e12fcf26902e60d9c0bd47
dohantaur <dohant.aurelien@gmail.com>
2013-11-07 15:51:25 +0100

    Default controller Ok

--- src/Guard/Api/v1Bundle/Controller/DefaultController.php
+++ src/Guard/Api/v1Bundle/Controller/DefaultController.php
@@ -4,26 +4,36 @@ namespace Guard\Api\v1Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Validator\Mapping\ClassMetadata;
-use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints as Upsert;
 
 class DefaultController extends Controller {
 
     public function indexAction() {
-        if ($this->getRequest() == 'POST') {
-            if (isJson($this->getRequest())){
-                $id = $this->getRequest()->id;
-                $state = $this->getRequest()->state;
-                $date = $this->getRequest()->date;
-                
-                $validator = $this->get('validator');
-                $errorList = $validator->validate($author);
-                
-                $this->get('logger')->info(id .','.state.','.date.'\n');
+        if ($this->getRequest()->isMethod("POST")) {
+            if ($this->isJson($this->getRequest()->getContent())) {
+                $idConstraint = new Upsert\Length(array(
+                    'min' => 36,
+                    'max' => 36
+                ));
+                $dateConstraint = new Upsert\DateTime();
+                $stateConstraint = new Upsert\Range(array(
+                    'min' => 0,
+                ));
+                $d = new \DateTime();
+                $request = json_decode($this->getRequest()->getContent());
+                $errorIdList = $this->get('validator')->validateValue($request->id, $idConstraint);
+                $errorDateList = $this->get('validator')->validateValue($d->setTimestamp($request->date), $dateConstraint);
+                $errorStateList = $this->get('validator')->validateValue($request->state, $stateConstraint);
+                if (count($errorIdList)!=0 || count($errorDateList)!=0 || count($errorStateList)!=0 || $d->setTimestamp($request->date) > new \DateTime('NOW')){ 
+                    return new response($errorDateList,500);
+                }
+                return new Response("", 200);
+            } else {
+                return new Response("", 404);
             }
+        } else {
+            return new Response("", 404);
         }
-        
-        return new Response("ok");
     }
 
     private function isJson($string) {
